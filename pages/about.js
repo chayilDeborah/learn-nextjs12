870ef81f3fe55bc61b9b82517bdbfd5906587a82
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-05-30 15:38:00 +0100

    feat: create pages to practice routing

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+// import '../styles/About.module.css'
+const About = () => {
+  return (
+    <div className='aboutus'>About</div>
+  )
+}
+
+export default About
---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Contact from './component/Contact'
+function ContactMe() {
+  return (
+    <div>
+        <Contact />
+        <div className='class'>IS Contact class workimg?</div>
+    </div>
+  )
+}
+export default  ContactMe
