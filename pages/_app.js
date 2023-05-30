46d4ad5086cd82fe81cb8a02cf1c89d198a60521
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-05-30 17:05:49 +0100

    feat: import layout to app root

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import Layout from './component/Layout'
 
+export const metadata = {
+  title: 'Learn NextJs12',
+  description: 'a repo for practicing Nextjs12',
+} 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( 
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+ )
 }
