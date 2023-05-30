b1aa94e2c563faec272ec3abffeb6ffc58a2a5bc
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-05-30 15:39:44 +0100

    feat: component folder  create contact component

---
+++ pages/component/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import contactStyles from '../../styles/Contact.module.css';
+
+const Contact = () => {
+  return (
+    <div className={contactStyles.contact}>Contact component</div>
+  )
+}
+
+export default Contact
